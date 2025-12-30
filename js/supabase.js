f181dc37de9ee27ee56f45552f05f312c3140fe7
Abdirahman <99055216+AbdirahmanDubad19@users.noreply.github.com>
2025-12-30 17:00:44 +0000

    added code

--- js/supabase.js
+++ js/supabase.js
@@ -1,2 +1,12 @@
 console.log("supabase config loaded");
 
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
+
+const SUPABASE_URL = "https://jjdcvwjfulyudbfbkdwx.supabase.co";
+const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_1zjYbj3nIeY0zAZQjiX3mw_1ELG3eyh";
+
+export const supabase = createClient(
+  SUPABASE_URL,
+  SUPABASE_PUBLISHABLE_KEY
+);
+
