ef33a070e78114a8becf5303da6188c66b1b5caf
Abdirahman <99055216+AbdirahmanDubad19@users.noreply.github.com>
2026-01-05 15:33:47 +0000

    adding my supabase ID and key

--- js/supabase.js
+++ js/supabase.js
@@ -1,8 +1,8 @@
 // supabase.js
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
-const supabaseUrl = "https://YOUR_PROJECT_REF.supabase.co"; // replace with your Supabase URL
-const supabaseKey = "YOUR_PUBLIC_ANON_KEY"; // replace with your anon public key
+const supabaseUrl = "https://jjdcvwjfulyudbfbkdwx.supabase.co"; // replace with your Supabase URL
+const supabaseKey = "sb_publishable_1zjYbj3nIeY0zAZQjiX3mw_1ELG3eyh"; // replace with your anon public key
 export const supabase = createClient(supabaseUrl, supabaseKey);
 
 
