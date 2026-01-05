fa08e7f9e3876758a70888f5321d06461a02af1b
Abdirahman <99055216+AbdirahmanDubad19@users.noreply.github.com>
2026-01-05 15:39:28 +0000

    fixing

--- js/supabase.js
+++ js/supabase.js
@@ -1,8 +1,8 @@
 // supabase.js
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
-const supabaseUrl = "https://jjdcvwjfulyudbfbkdwx.supabase.co"; // replace with your Supabase URL
-const supabaseKey = "sb_publishable_1zjYbj3nIeY0zAZQjiX3mw_1ELG3eyh"; // replace with your anon public key
+const supabaseUrl = "https://jjdcvwjfulyudbfbkdwx.supabase.co"; // Supabase URL
+const supabaseKey = "sb_publishable_1zjYbj3nIeY0zAZQjiX3mw_1ELG3eyh"; // anon public key
 export const supabase = createClient(supabaseUrl, supabaseKey);
 
 
