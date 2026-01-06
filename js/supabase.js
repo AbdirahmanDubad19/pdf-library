6c8c8a72d90892a3f11e4224c397fc45bb820fff
Abdirahman <99055216+AbdirahmanDubad19@users.noreply.github.com>
2026-01-06 13:55:35 +0000

    fixing supabase import in HTML files

--- js/supabase.js
+++ js/supabase.js
@@ -1,8 +1,4 @@
-// js/supabase.js
-import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2.31.0/dist/supabase.min.js";
-
-// Your Supabase project info
-const SUPABASE_URL = "https://jjdcvwjfulyudbfbkdwx.supabase.co";
-const SUPABASE_KEY = "sb_publishable_1zjYbj3nIeY0zAZQjiX3mw_1ELG3eyh";
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
+export const supabase = supabase.createClient(
+  "https://jjdcvwjfulyudbfbkdwx.supabase.co",
+  "sb_publishable_1zjYbj3nIeY0zAZQjiX3mw_1ELG3eyh"
+);
