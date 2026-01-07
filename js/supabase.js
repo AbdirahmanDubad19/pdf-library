040b02f823b68bf8a38f5867af1f0a58fb21110e
Abdirahman <99055216+AbdirahmanDubad19@users.noreply.github.com>
2026-01-07 10:57:15 +0000

    fixing log in

--- js/supabase.js
+++ js/supabase.js
@@ -1,6 +1,4 @@
 // js/supabase.js
-
-// use the global `supabase` created by the CDN
 export const supabaseClient = supabase.createClient(
   "https://jjdcvwjfulyudbfbkdwx.supabase.co",
   "sb_publishable_1zjYbj3nIeY0zAZQjiX3mw_1ELG3eyh"
